6b8c7a9232b4322a6eefc4e7da206dd951280379
Sharjeel <iasharjeel@gmail.com>
2025-01-05 01:05:40 +0530

    server side validation and dark theme

--- eVillaBooking.Domain/Entities/Villa.cs
+++ eVillaBooking.Domain/Entities/Villa.cs
@@ -1,11 +1,15 @@
-﻿namespace eVillaBooking.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eVillaBooking.Domain.Entities
 {
     public class Villa
     {
         public int Id {  get; set; }
+        [MaxLength(30)]
         public required string  Name { get; set; }
         public string? Description { get; set; }
         public double Price { get; set; }
+        [Range(10,10000)]
         public int Sqft { get; set; }
         public int Occupancy {  get; set; }
         public string? ImageUrl {  get; set; }
---
+++ eVillaBooking.Presentation/Controllers/VillaController.cs
@@ -0,0 +1,36 @@
+﻿using eVillaBooking.Domain.Entities;
+using eVillaBooking.Infrastructher.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eVillaBooking.Presentation.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.MyProperty.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.MyProperty.Add(villa);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(villa);
+        }
+    }
+}
+ 
