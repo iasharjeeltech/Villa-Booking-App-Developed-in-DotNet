471823f16252ac5a0d4d2e07e51ddf4ce7181662
Sharjeel <iasharjeel@gmail.com>
2025-01-05 15:04:20 +0530

    CRUD and others functionality integrated successfully!

--- eVillaBooking.Presentation/Controllers/VillaController.cs
+++ eVillaBooking.Presentation/Controllers/VillaController.cs
@@ -23,6 +23,10 @@ namespace eVillaBooking.Presentation.Controllers
         [HttpPost]
         public IActionResult Create(Villa villa)
         {
+            if(villa.Name == villa.Description)
+            {
+                ModelState.AddModelError("Name","Name & Description are same!");
+            }
             if (ModelState.IsValid)
             {
                 _db.MyProperty.Add(villa);
@@ -31,6 +35,59 @@ namespace eVillaBooking.Presentation.Controllers
             }
             return View(villa);
         }
+
+        public IActionResult Edit(int id)
+        {
+            var villa = _db.MyProperty.Find(id);
+            //var villa1 = _db.MyProperty.FirstOrDefault(v => v.Id == id);
+            //var villa1 = _db.MyProperty.SingleOrDefault(v => v.Id == id);
+
+            if (villa == null)
+            {
+                return RedirectToAction("Error","Home");
+            }
+            return View(villa);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Villa villa)
+        {
+            if (ModelState.IsValid) 
+            {
+                _db.MyProperty.Update(villa);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(villa);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var villa = _db.MyProperty.Find(id);
+            if(villa is null)
+            {
+                return NotFound();
+            }
+            return View(villa);
+        }
+
+        //[HttpPost]
+        //public IActionResult Delete (Villa villa)
+        //{
+        //    _db.MyProperty.Remove(villa);
+        //    _db.SaveChanges();
+        //    return RedirectToAction(nameof(Index));
+        //}
+        //the above code is working properly 
+
+        [HttpPost]
+        public IActionResult DeleteConfirm (int id)
+        {
+            var villa = _db.MyProperty.Find(id);
+            _db.MyProperty.Remove(villa);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
- 
